d08071f79678a539e0b39879217f1867fbd9f211
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-26 08:43:30 -0300

    Requisito 3: Aprovado pelo Stryker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+test(`Teste se é exibida na tela a mensagem "No favorite pokemon found", caso a pessoa
+ não tenha pokémons favoritos`, () => {
+  renderWithRouter(<FavoritePokemons />);
+  const text = /No favorite pokemon found/i;
+  const favoriteLink = screen.getByText(text);
+  expect(favoriteLink).toBeInTheDocument();
+});
