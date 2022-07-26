126bd35393a2102bf0304facc09fca0ca1ebf889
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-26 08:24:08 -0300

    Requisito 1: Aprovado pelo Stryker

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Teste o componente <App.js />', () => {
+  it('O primeiro link deve possuir o texto "Home"', () => {
+    renderWithRouter(<App />);
+    const expectedHome = 'Home';
+    const linkHome = screen.getByRole('link', { name: expectedHome });
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto "About"', () => {
+    renderWithRouter(<App />);
+    const expectedText = 'About';
+    const linkAbout = screen.getByRole('link', { name: expectedText });
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+    const expectedFavorite = 'Favorite Pokémons';
+    const linkFavorite = screen.getByRole('link', { name: expectedFavorite });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
